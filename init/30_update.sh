a2d7cd6c57799cd284f1c2900546040d2d29f458
sparklyballs <sparklyballs@gmail.com>
2016-05-28 16:40:53 +0100

    update to new git for couch and change to chown -R abc:abc after git pull

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-[[ ! -d /app/couchpotato/.git ]] && /sbin/setuser abc git clone https://github.com/RuudBurger/CouchPotatoServer.git /app/couchpotato
+
+[[ ! -d /app/couchpotato/.git ]] && git clone \
+https://github.com/CouchPotato/CouchPotatoServer /app/couchpotato
+
+chown -R abc:abc /app/couchpotato
 
 # opt out for autoupdates
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
 cd /app/couchpotato
-/sbin/setuser abc git pull
-
+git pull
+chown -R abc:abc /app/couchpotato
